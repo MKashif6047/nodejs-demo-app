af9e9dadf369de01d1020ad9c60b8377bb4dbf2a
Shaik kashif <mohammedkashifshaik741@gmail.com>
2026-08-27 13:32:16 +0530

    Initial Node.js application

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  res.end(`
+    <h1>Hello from Node.js!</h1>
+    <p>DevOps Internship Demo Application</p>
+  `);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
